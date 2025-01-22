e9901415b3192c3c9606a97b4a3ecb109f001d79
Mathis Borkowska <mathisborkowska@gmail.com>
2025-01-22 14:44:47 +0100

    Définition de variables

---
+++ exercices.js
@@ -0,0 +1,7 @@
+///exercice: Définition de variables
+
+///1.1
+let nom_boutique = "L'Apothicaire"
+let nombre_potion_de_soin_en_stock = 12
+let prix_potion_de_soin = 5.99
+let status_boutique = ["ouvet","fermer"]

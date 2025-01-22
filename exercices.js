8460c450c2a10b708292108f5ce5e59b4f1220dc
Mathis Borkowska <mathisborkowska@gmail.com>
2025-01-22 14:55:21 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -1,7 +1,15 @@
 ///exercice: Définition de variables
 
-///1.1
 let nom_boutique = "L'Apothicaire"
 let nombre_potion_de_soin_en_stock = 12
 let prix_potion_de_soin = 5.99
-let status_boutique = ["ouvet","fermer"]
+let status_boutique = "fermer"
+
+///exercice: Affichage conditionnel
+
+if (status_boutique == "ouvet"){
+    console.log("bienvenue dans la boutique" + nom_boutique + "aventurier !")
+}
+else if (status_boutique == "fermer"){
+    console.log("La boutique " + nom_boutique + " est fermée, revenez plus tard Aventurier ! 😴")
+}
